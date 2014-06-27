078b45719bf36f45b571d5ada6fbc1daf9bc3138
Ismail Bougnouch <ismail00b@gmail.com>
2014-06-27 16:45:28 +0200

    Update #3

---
+++ Untitled1/Widgets/CustomWG/designer.js
@@ -0,0 +1,41 @@
+(function(CustomWG) {
+
+//    /* Default width and height of your widget */
+//    CustomWG.setWidth('200');
+//    CustomWG.setHeight('20');
+
+//    /* Define custom event for your widget */
+//    CustomWG.addEvent('myEvent');
+
+//    /* Customize existing properties */
+//    CustomWG.customizeProperty('test', {
+//        sourceTitle: 'Test Source',
+//        title: 'Test Static Value',
+//        description: 'Add a datasource to this property.'
+//    });
+
+//    /* Add a Label property */
+//    CustomWG.addLabel({
+//        'defaultValue': '',
+//        'position': 'top'
+//    });
+
+//    /* Set the Design and Styles panels */
+//    CustomWG.setPanelStyle({
+//        'fClass': true,
+//        'text': true,
+//        'background': true,
+//        'border': true,
+//        'sizePosition': true,
+//        'label': true,
+//        'disabled': ['border-radius']
+//    });
+
+//    /* Override widget's initialization */
+//    CustomWG.prototype.init = function() {
+//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+//    }
+
+});
+
+// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3870.html
---
+++ Untitled1/Widgets/CustomWG/widget.js
@@ -0,0 +1,29 @@
+WAF.define('CustomWG', ['waf-core/widget'], function(widget) {
+	
+    var CustomWG = widget.create('CustomWG', {
+        init: function() {
+//            /* Define a custom event */
+//            this.fire('myEvent', {
+//                message: 'Hello'
+//            });
+        }
+//        ,
+        
+//        /* Create a property */
+//        test: widget.property({
+//            onChange: function(newValue) {
+//                this.node.innerHTML = this.test(); /* this contains the widget and newValue contains its current value */
+//            }
+//        })
+    });
+
+//    /* Map the custom event above to the DOM click event */
+//    CustomWG.mapDomEvents({
+//        'click': 'action'
+//    });
+
+    return CustomWG;
+
+});
+
+/* For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html */
